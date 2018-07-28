e316955b98f2068c5b06c0493e1dc7e2b523a464
qinfeng8848 <q884822@gmail.com>
2018-07-28 23:24:06 +0800

    完成电影的CRUD及页面

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,55 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    flash[:alert] = "电影已经删除"
+    redirect_to movies_path
+
+  end
+
+
+
+
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,:description)
+  end
+
 end
