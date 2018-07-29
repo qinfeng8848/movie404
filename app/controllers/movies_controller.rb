152931ef54e7b0c69a6f3ae87c69bbdb52afdc6b
qinfeng8848 <q884822@gmail.com>
2018-07-29 21:52:31 +0800

    用户建立电影后,自动收藏

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,7 +32,9 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     @movie.user = current_user
+
     if @movie.save
+      current_user.join!(@movie)
       redirect_to movies_path
     else
       render :new
